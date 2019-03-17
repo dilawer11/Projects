45603fc2ab319e772a915562ecc797191dc88375
Dilawer Ahmed <dilawerahmed@Dilawers-MacBook-Pro.local>
2019-03-17 18:41:22 +0500

    Edit Changes

--- DataFlow/Dataflow/dataflow.cpp
+++ DataFlow/Dataflow/dataflow.cpp
@@ -5,7 +5,20 @@
 #include "dataflow.h"
 
 namespace llvm {
-  
+  BitVector DataFlow::runMeetOp(vector<BitVector> bitVectors);
+  {
+    BitVector returnBV;
+    for(int i=0;i<bitVectors.size();i++){
+        if(meetOp){
+            returnBV |=bitVectors[i];
+        }
+        else{
+            returnBV &=bitVectors[i];
+        }
+    }
+    return returnBV;
+  }
+  void DataFlow::runPass(){}
 
   // Add code for your dataflow abstraction here.
 }
--- DataFlow/Dataflow/dataflow.h
+++ DataFlow/Dataflow/dataflow.h
@@ -15,13 +15,58 @@
 #include "llvm/IR/CFG.h"
 
 namespace llvm {
-    bool direction; //true for forwards, false for backwards
-    BitVector<void*> domain;
-    bool meetOp;  //true for union, false for intersection
-    //initial interior points
-    //boundary positions
-    //transfer function
+    struct BasicBlockSt{
+        vector<BasicBlockSt*> prev;
+        BitVector in;
+        BitVector out=boundaryCondition;
+    };
+    struct DFResult{
 
+    };
+
+    class DataFlow{
+    public:
+        
+    
+    //variables
+    bool direction; //true for forwards, false for backwards
+    std::vector<void*> rawDomain;
+    bool meetOp;  //true for union, false for intersection
+    BitVector initialCondition;
+    BitVector boundaryCondition;
+    std::map<BasicBlock*,BasicBlockSt> BlockMap;
+
+    //methods
+    BitVector runMeetOp(vector<BitVector> bitVectors);
+    void runPass(Function &F){
+        //setup Prev List
+        for (Function::iterator FI = F.begin(), FE = F.end(); FI != FE; ++FI) {
+	        BasicBlock* block = &*FI;
+            if(direction){
+                for(pred_iterator PI = pred_begin(BB); PI != pred_end(BB); ++PI){
+                    BlockMap[block].prev.push_back(*PI);
+                }
+            }
+            else{
+                for(succ_iterator SI = succ_begin(BB); SI != succ_end(BB); ++SI){
+                    BlockMap[block].prev.push_back(*SI);
+                }
+            }
+        }
+        // for(std::map<BasicBlock*,BasicBlockSt::iterator it =BlockMap.begin();it!=BlockMap.end();++it){
+        //     it->second.out=boundaryCondition;
+        // }
+        for(std::map<BasicBlock*,BasicBlockSt>::iterator it = BlockMap.begin();it!=BlockMap.end();++it){
+            if(it->second.prev.size()==0){
+                it->second.prev.in=initialCondition;
+                outs() << "Found Initial Basic Block\n"; //Remove this later
+            }
+        }
+    }
+
+
+    
+    };
 
 // Add definitions (and code, depending on your strategy) for your dataflow
 // abstraction here.
